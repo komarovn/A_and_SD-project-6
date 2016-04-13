58300ff39457c40f2e1b8038def9b6158386dbcf
Комаров Николай Александрович <komarov.n@ccam.cmc.unn.net>
2016-04-13 10:41:23 +0300

    Form initialized

--- Project-5-Test/Project-5-Test/MyForm.h
+++ Project-5-Test/Project-5-Test/MyForm.h
@@ -34,6 +34,10 @@ namespace Project5Test {
 				delete components;
 			}
 		}
+	private: System::Windows::Forms::Button^  button1;
+	private: System::Windows::Forms::DataGridView^  dataGridView1;
+	private: System::Windows::Forms::PictureBox^  pictureBox1;
+	protected: 
 
 	private:
 		/// <summary>
@@ -48,12 +52,80 @@ namespace Project5Test {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->dataGridView1 = (gcnew System::Windows::Forms::DataGridView());
+			this->pictureBox1 = (gcnew System::Windows::Forms::PictureBox());
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dataGridView1))->BeginInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->BeginInit();
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(736, 457);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"Выход";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// dataGridView1
+			// 
+			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
+			this->dataGridView1->Location = System::Drawing::Point(578, 12);
+			this->dataGridView1->Name = L"dataGridView1";
+			this->dataGridView1->Size = System::Drawing::Size(233, 439);
+			this->dataGridView1->TabIndex = 1;
+			this->dataGridView1->ColumnCount = 4;
+			this->dataGridView1->RowCount = 1;
+
+			DataGridViewColumn^ column0 = dataGridView1->Columns[0];
+			column0->Width = 50;
+			column0->HeaderText = "Имя";
+			DataGridViewColumn^ column1 = dataGridView1->Columns[1];
+			column1->Width = 50;
+			column1->HeaderText = "x";
+			DataGridViewColumn^ column2 = dataGridView1->Columns[2];
+			column2->Width = 50;
+			column2->HeaderText = "y";
+			DataGridViewColumn^ column3 = dataGridView1->Columns[3];
+			column3->Width = 50;
+			column3->HeaderText = "Адрес";
+			// 
+			// pictureBox1
+			// 
+			this->pictureBox1->Location = System::Drawing::Point(12, 12);
+			this->pictureBox1->Name = L"pictureBox1";
+			this->pictureBox1->Size = System::Drawing::Size(560, 439);
+			this->pictureBox1->TabIndex = 2;
+			this->pictureBox1->TabStop = false;
+			this->pictureBox1->MouseClick += gcnew System::Windows::Forms::MouseEventHandler(this, &MyForm::pictureBox1_MouseClick);
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(823, 492);
+			this->Controls->Add(this->pictureBox1);
+			this->Controls->Add(this->dataGridView1);
+			this->Controls->Add(this->button1);
+			this->Cursor = System::Windows::Forms::Cursors::Default;
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
+			this->Name = L"MyForm";
+			this->Text = L"Drawing Points";
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dataGridView1))->EndInit();
+			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				 Close();
+			 }
+	private: System::Void pictureBox1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
+				 int x = e->X;
+				 int y = e->Y;
+
+			 }
 	};
 }
