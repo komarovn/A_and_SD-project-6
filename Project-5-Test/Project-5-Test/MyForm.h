b2cc37bb723875f1e4ac516656f5d454802e9efc
Комаров Николай Александрович <komarov.n@ccam.cmc.unn.net>
2016-04-13 11:13:08 +0300

    FFF

--- Project-5-Test/Project-5-Test/MyForm.h
+++ Project-5-Test/Project-5-Test/MyForm.h
@@ -61,7 +61,7 @@ namespace Project5Test {
 			// 
 			// button1
 			// 
-			this->button1->Location = System::Drawing::Point(736, 457);
+			this->button1->Location = System::Drawing::Point(761, 457);
 			this->button1->Name = L"button1";
 			this->button1->Size = System::Drawing::Size(75, 23);
 			this->button1->TabIndex = 0;
@@ -71,29 +71,23 @@ namespace Project5Test {
 			// 
 			// dataGridView1
 			// 
+			this->dataGridView1->AllowUserToAddRows = false;
+			this->dataGridView1->AllowUserToDeleteRows = false;
+			this->dataGridView1->AllowUserToResizeColumns = false;
+			this->dataGridView1->AllowUserToResizeRows = false;
+			this->dataGridView1->BackgroundColor = System::Drawing::SystemColors::Control;
+			this->dataGridView1->BorderStyle = System::Windows::Forms::BorderStyle::None;
 			this->dataGridView1->ColumnHeadersHeightSizeMode = System::Windows::Forms::DataGridViewColumnHeadersHeightSizeMode::AutoSize;
 			this->dataGridView1->Location = System::Drawing::Point(578, 12);
 			this->dataGridView1->Name = L"dataGridView1";
-			this->dataGridView1->Size = System::Drawing::Size(233, 439);
+			this->dataGridView1->ReadOnly = true;
+			this->dataGridView1->RowHeadersVisible = false;
+			this->dataGridView1->Size = System::Drawing::Size(258, 439);
 			this->dataGridView1->TabIndex = 1;
-			this->dataGridView1->ColumnCount = 4;
-			this->dataGridView1->RowCount = 1;
-
-			DataGridViewColumn^ column0 = dataGridView1->Columns[0];
-			column0->Width = 50;
-			column0->HeaderText = "Имя";
-			DataGridViewColumn^ column1 = dataGridView1->Columns[1];
-			column1->Width = 50;
-			column1->HeaderText = "x";
-			DataGridViewColumn^ column2 = dataGridView1->Columns[2];
-			column2->Width = 50;
-			column2->HeaderText = "y";
-			DataGridViewColumn^ column3 = dataGridView1->Columns[3];
-			column3->Width = 50;
-			column3->HeaderText = "Адрес";
 			// 
 			// pictureBox1
 			// 
+			this->pictureBox1->BorderStyle = System::Windows::Forms::BorderStyle::FixedSingle;
 			this->pictureBox1->Location = System::Drawing::Point(12, 12);
 			this->pictureBox1->Name = L"pictureBox1";
 			this->pictureBox1->Size = System::Drawing::Size(560, 439);
@@ -105,7 +99,7 @@ namespace Project5Test {
 			// 
 			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(823, 492);
+			this->ClientSize = System::Drawing::Size(848, 492);
 			this->Controls->Add(this->pictureBox1);
 			this->Controls->Add(this->dataGridView1);
 			this->Controls->Add(this->button1);
@@ -113,6 +107,7 @@ namespace Project5Test {
 			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::FixedSingle;
 			this->Name = L"MyForm";
 			this->Text = L"Drawing Points";
+			this->Load += gcnew System::EventHandler(this, &MyForm::MyForm_Load);
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->dataGridView1))->EndInit();
 			(cli::safe_cast<System::ComponentModel::ISupportInitialize^  >(this->pictureBox1))->EndInit();
 			this->ResumeLayout(false);
@@ -120,12 +115,33 @@ namespace Project5Test {
 		}
 #pragma endregion
 	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
-				 Close();
+				Close();
 			 }
 	private: System::Void pictureBox1_MouseClick(System::Object^  sender, System::Windows::Forms::MouseEventArgs^  e) {
-				 int x = e->X;
-				 int y = e->Y;
+				int x = e->X;
+				int y = e->Y;
 
+				dataGridView1->RowCount += 1;
+
+				dataGridView1[1, dataGridView1->RowCount - 1]->Value = x;
+				dataGridView1[2, dataGridView1->RowCount - 1]->Value = y;
 			 }
-	};
+	private: System::Void MyForm_Load(System::Object^  sender, System::EventArgs^  e) {
+				 dataGridView1->ColumnCount = 4;
+				dataGridView1->RowCount = 0;
+
+				DataGridViewColumn^ column0 = dataGridView1->Columns[0];
+				column0->Width = 40;
+				column0->HeaderText = "Имя";
+				DataGridViewColumn^ column1 = dataGridView1->Columns[1];
+				column1->Width = 50;
+				column1->HeaderText = "x";
+				DataGridViewColumn^ column2 = dataGridView1->Columns[2];
+				column2->Width = 50;
+				column2->HeaderText = "y";
+				DataGridViewColumn^ column3 = dataGridView1->Columns[3];
+				column3->Width = 90;
+				column3->HeaderText = "Адрес";
+			 }
+};
 }
