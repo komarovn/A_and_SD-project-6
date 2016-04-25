25b8b3f5075ff8c2db3c6a8aeea4eb6d3d3740c0
Komarov Nikolay <komarov.nikolay.russia@gmail.com>
2016-04-25 10:59:47 +0300

    PointC completed

--- Project-5-Test/Project-5-Test/MyForm.h
+++ Project-5-Test/Project-5-Test/MyForm.h
@@ -25,15 +25,9 @@ namespace Project5Test {
 		MyForm(void)
 		{
 			InitializeComponent();
-			//
-			//TODO: Add the constructor code here
-			//
 		}
 
 	protected:
-		/// <summary>
-		/// Clean up any resources being used.
-		/// </summary>
 		~MyForm()
 		{
 			if (components)
@@ -47,16 +41,9 @@ namespace Project5Test {
 	protected: 
 
 	private:
-		/// <summary>
-		/// Required designer variable.
-		/// </summary>
 		System::ComponentModel::Container ^components;
 
 #pragma region Windows Form Designer generated code
-		/// <summary>
-		/// Required method for Designer support - do not modify
-		/// the contents of this method with the code editor.
-		/// </summary>
 		void InitializeComponent(void)
 		{
 			this->button1 = (gcnew System::Windows::Forms::Button());
--- Project-5-Test/Project-5-Test/PointClass.h
+++ Project-5-Test/Project-5-Test/PointClass.h
@@ -10,7 +10,17 @@ private:
 	int yCoord;
 public:
 	PointC(string Name = "", int X = 0, int Y = 0, Color c = Color::Black, int Size = 3);
-	
+	PointC(const PointC &p);
+	~PointC() {};
+	PointC &operator=(const PointC &p);
+	void Draw(Graphics^ g);
+	void Move(int dx, int dy);
+	void SetNewPlace(int x, int y);
+	void AddRef();
+	void DelRef();
+	int Distance(int x, int y);
+	int GetX() const {return xCoord;};
+	int GetY() const {return yCoord;};
 };
 
 PointC::PointC(string Name, int X, int Y, Color c, int Size)
@@ -23,6 +33,68 @@ PointC::PointC(string Name, int X, int Y, Color c, int Size)
 	visible - true;
 	SetSize(Size);
 	refCount = 1;
+}
+
+PointC::PointC(const PointC &p)
+{
+	type = TPoint;
+	name = p.name;
+	xCoord = p.xCoord;
+	yCoord = p.yCoord;
+	color = p.color;
+	visible = 1;
+	SetSize(p.size);
+	refCount = 1;
+}
+
+PointC &PointC::operator=(const PointC &p)
+{
+	type = TPoint;
+	name = p.name;
+	xCoord = p.xCoord;
+	yCoord = p.yCoord;
+	color = p.color;
+	visible = 1;
+	SetSize(p.size);
+	refCount = 1;
+	return *this;
+}
+
+void PointC::Draw(Graphics^ g)
+{
+	g->FillEllipse(Brushes::Black, xCoord, yCoord, 2 * size, 2 * size);
+	g->DrawString(Convert::ToString(name.c_str()), gcnew System::Drawing::Font("Arial", 11), System::Drawing::Brushes::Blue, Point(xCoord, yCoord));
+}
+
+void PointC::Move(int dx, int dy)
+{
+	xCoord += dx;
+	yCoord += dy;
+}
+
+void PointC::SetNewPlace(int x, int y)
+{
+	xCoord = x;
+	yCoord = y;
+}
+
+void PointC::AddRef()
+{
+	refCount++;
+}
+
+void PointC::DelRef()
+{
+	refCount--;
+}
+
+int PointC::Distance(int x, int y)
+{
+	int distance = (x - xCoord) * (x - xCoord) + (y - yCoord) * (y - yCoord);
+	if (distance < epsilon * epsilon)
+		return 1;
+	else
+		return 0;
 }
 
 #endif
